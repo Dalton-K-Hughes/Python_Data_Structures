3f11ffc7cc2135ef10806a58009ee921e010efc9
Dalton Hughes <hughesda@lee.k12.ga.us>
2024-10-18 12:41:10 -0400

    Added the __iter__ function to the linkedlist to traverse it

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -1,3 +1,7 @@
+'''
+    Node will represent an element inside a linked list
+'''
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -6,9 +10,25 @@ class Node:
     def __repr__(self):
         return self.data
 
+
+'''
+    A linked list data structure
+'''
 class LinkedList:
-    def __init__(self):
+    def __init__(self, nodes=None):
         self.head = None
+        if nodes is not None:
+            node = Node(data=nodes.pop(0))
+            self.head = node
+            for elem in nodes:
+                node.next = Node(data=elem)
+                node = node.next
+
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
 
     def __repr__(self):
         node = self.head
