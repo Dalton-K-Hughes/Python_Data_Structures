f157c8f3baeab720074e0f8a9a24bc2134e13b29
Dalton Hughes <hughesda@lee.k12.ga.us>
2024-10-21 12:47:11 -0400

    Added functionality for adding items to the end of the linked list

--- LinkedList/Linked_List.py
+++ LinkedList/Linked_List.py
@@ -13,8 +13,9 @@ class LinkedList:
         self.head = None
 
     def add_first(self, data):
-        #TODO Add functionality for adding items to the front of the list
+
         node = Node(data)
+
         if self.head is None:
             self.head = node
         else:
@@ -22,8 +23,16 @@ class LinkedList:
             self.head = node
 
     def add_last(self, data):
-        #TODO Add functionality for adding items to the end of the list
-        pass
+
+        node = Node(data)
+
+        if self.head is None:
+            self.head = none
+        else:
+            current_node = self.head
+            while current_node.next is not None:
+                current_node = current_node.next
+            current_node.next = node
 
     def remove_item(self, item):
         #TODO Add functionality to remove items from the list 
