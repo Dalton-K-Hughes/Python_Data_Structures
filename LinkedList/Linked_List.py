6fb4f341488217806a92b06ca0007bfdd98455a3
Dalton Hughes <hughesda@lee.k12.ga.us>
2024-11-05 12:23:06 -0500

    Added functionailty for adding an item to the list at a specified index and fixed some errors in the code

--- LinkedList/Linked_List.py
+++ LinkedList/Linked_List.py
@@ -11,6 +11,7 @@ class LinkedList:
 
     def __init__(self):
         self.head = None
+        self.size = 0
 
     def add_first(self, data):
 
@@ -22,21 +23,57 @@ class LinkedList:
             node.next = self.head
             self.head = node
 
+        self.size += 1
+
     def add_last(self, data):
 
         node = Node(data)
 
         if self.head is None:
-            self.head = None 
+            self.head = node
+
         else:
             current_node = self.head
             while current_node.next is not None:
                 current_node = current_node.next
             current_node.next = node
+        
+        self.size += 1
 
     def add_item(self, index, data):
-        #TODO: Add functionailty to add an item at a specified index in the list
-        pass
+
+        if self.size < index:
+            print(f'Cannot add {data} at index {index}')
+            return
+
+        if self.size == index:
+            node = Node(data)
+            self.add_last(node)
+            self.size += 1
+            return
+
+        if self.head is None and index == 0:
+            node = Node(data)
+            self.head = node
+            self.size += 1
+            return
+
+        if index == 0:
+            node = Node(data)
+            node.next = self.head
+            self.head = node
+            self.size += 1
+            return
+
+        current_node = self.head
+        for i in range(index):
+            prev_node = current_node
+            current_node = current_node.next
+
+        node = Node(data)
+        prev_node.next = node
+        node.next = current_node
+        self.size += 1
 
     def peek(self):
         if self.head is not None:
@@ -49,9 +86,12 @@ class LinkedList:
         
         if self.head.next is None:
             self.head = None
+            self.size -= 1
             return
 
         self.head = self.head.next
+        self.size -= 1
+
 
     def remove_last(self):
         if self.head is None:
@@ -60,6 +100,7 @@ class LinkedList:
         
         if self.head.next is None:
             self.head = None
+            self.size -= 1
             return
 
         current_node = self.head
@@ -76,6 +117,7 @@ class LinkedList:
         if self.head.data == item:
             data = self.head.data
             self.head = self.head.next
+            self.size -= 1
             return data
         
         current_node = self.head
@@ -85,6 +127,7 @@ class LinkedList:
             if current_node.data == item:
                 data = current_node.data
                 prev_node.next = current_node.next
+                self.size -= 1
                 return data
         print('Item not found in list')
         return
@@ -97,7 +140,7 @@ class LinkedList:
     def __repr__(self):
         list = []
         current_node = self.head
-        while current_node:
+        while current_node is not None:
             list.append(str(current_node.data))
             current_node = current_node.next
         list.append('None')
