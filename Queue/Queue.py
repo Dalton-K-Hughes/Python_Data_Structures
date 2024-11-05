426f9d8d800c992dcdd661024c4db5f58eed863b
Dalton Hughes <hughesda@lee.k12.ga.us>
2024-11-05 13:29:27 -0500

    Added a node class to the queue and added a queue class with functiona definitions with todos to finish each function

---
+++ Queue/Queue.py
@@ -0,0 +1,34 @@
+class Node:
+
+    def init(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return self.data
+
+class Queue:
+
+    def init(self):
+        #TODO: Add functionalty to init an empty queue
+        pass
+
+    def peek(self):
+        #TODO: Add functionality to look at the first item in the queue
+        pass
+
+    def pop(self):
+        #TODO: Add functionality to remove the first item in the queue
+        pass
+
+    def add_last(self, item):
+        #TODO: Add functionailty to add an item to the end of the queue
+        pass
+
+    def __iter__(self):
+        #TODO: Add functionality to iterate through the queue
+        pass
+
+    def __repr__(self):
+        #TODO: Add functionality to show the queue
+        pass
