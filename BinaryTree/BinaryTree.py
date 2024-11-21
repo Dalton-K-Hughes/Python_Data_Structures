be636a699996a9b933a95f8928ba5cbb19a8e01b
Dalton Hughes <hughesda@lee.k12.ga.us>
2024-11-21 10:02:33 -0500

    Created the node class for the binary tree along with the binary tree class with function definitions with todos to complete

---
+++ BinaryTree/BinaryTree.py
@@ -0,0 +1,32 @@
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        return self.data
+
+class BinaryTree:
+    def __init__(self):
+        #TODO: Create functionailty to initliaze a binary tree
+        pass
+
+    def insert(self, data):
+        #TODO: Create functionaility to insert an item into the tree
+        pass
+
+    def remove(self, data):
+        #TODO: Create functionality to remove an item from the tree
+        pass
+
+    def __repr__(self):
+        #TODO: create functionailty to return a string representation of the binary tree
+        pass
+
+    def __iter__(self):
+        #TODO: Create functionailty to create a interator function for the tree
+        pass
+
+    
